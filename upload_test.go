22bfe1350a179e1a3d66cda86ebc623e07c65905
omerzamir <zamir98@gmail.com>
2019-03-19 11:57:33 +0200

    changed test to run with buffconn to mock grpc connection
    Signed-off-by: omerzamir <zamir98@gmail.com>

--- upload_test.go
+++ upload_test.go
@@ -3,24 +3,35 @@ package main
 import (
 	"bytes"
 	"context"
+	"crypto/rand"
 	"fmt"
 	"io"
+	"log"
+	"net"
 	"os"
 	"reflect"
 	"testing"
+	"time"
 	pb "upload-service/proto"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/test/bufconn"
 )
 
+const bufSize = 1024 * 1024
+
+// Declaring global variables.
 var s3Endpoint string
 var newSession = session.Must(session.NewSession())
 var s3Client *s3.S3
+var lis *bufconn.Listener
 
 func init() {
+	// Fetch env vars
 	s3AccessKey := os.Getenv("S3_ACCESS_KEY")
 	s3SecretKey := os.Getenv("S3_SECRET_KEY")
 	s3Endpoint = os.Getenv("S3_ENDPOINT")
@@ -34,8 +45,24 @@ func init() {
 		DisableSSL:       aws.Bool(true),
 		S3ForcePathStyle: aws.Bool(true),
 	}
+
+	// Init real client.
 	newSession = session.New(s3Config)
 	s3Client = s3.New(newSession)
+
+	lis = bufconn.Listen(bufSize)
+	grpcServer := grpc.NewServer(grpc.MaxRecvMsgSize(10000000))
+	server := &UploadHandler{UploadService: UploadService{s3Client: s3Client}}
+	pb.RegisterUploadServer(grpcServer, server)
+	go func() {
+		if err := grpcServer.Serve(lis); err != nil {
+			log.Fatalf("Server exited with error: %v", err)
+		}
+	}()
+}
+
+func bufDialer(string, time.Duration) (net.Conn, error) {
+	return lis.Dial()
 }
 
 func TestUploadService_UploadFile(t *testing.T) {
@@ -147,6 +174,9 @@ func TestUploadService_UploadFile(t *testing.T) {
 }
 
 func TestUploadHandler_UploadMedia(t *testing.T) {
+	hugefile := make([]byte, 5000000)
+	rand.Read(hugefile)
+
 	uploadservice := UploadService{
 		s3Client: s3Client,
 	}
@@ -222,13 +252,39 @@ func TestUploadHandler_UploadMedia(t *testing.T) {
 				Output: fmt.Sprintf("%s/testbucket/testfile.txt", s3Endpoint),
 			},
 		},
+		{
+			name:   "UploadMedia - text file - huge file",
+			fields: fields{UploadService: uploadservice},
+			args: args{
+				ctx: context.Background(),
+				request: &pb.UploadMediaRequest{
+					Key:    "testfile.txt",
+					Bucket: "testbucket",
+					File:   hugefile,
+				},
+			},
+			wantErr: false,
+			want: &pb.UploadMediaResponse{
+				Output: fmt.Sprintf("%s/testbucket/testfile.txt", s3Endpoint),
+			},
+		},
 	}
+
+	// Create connection to server
+	ctx := context.Background()
+	conn, err := grpc.DialContext(ctx, "bufnet", grpc.WithDialer(bufDialer), grpc.WithInsecure())
+	if err != nil {
+		t.Fatalf("Failed to dial bufnet: %v", err)
+	}
+	defer conn.Close()
+
+	// Create client
+	client := pb.NewUploadClient(conn)
+
+	// Iterate over test cases
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			h := UploadHandler{
-				UploadService: tt.fields.UploadService,
-			}
-			got, err := h.UploadMedia(tt.args.ctx, tt.args.request)
+			got, err := client.UploadMedia(tt.args.ctx, tt.args.request)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("UploadHandler.UploadMedia() error = %v, wantErr %v", err, tt.wantErr)
 				return
