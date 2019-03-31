4c91b890dc6510a169e25cb2f426fc4c8a5a2e9f
Omer Zamir <zamir98@gmail.com>
2019-03-31 16:46:35 +0300

    Develop (#3) - Sprint 1
    * initial upload service commit
    
    Signed-off-by: Omri Shtamberger <omrishtam@gmail.com>
    
    * added initial proto file of the upload service
    
    Signed-off-by: omerzamir <zamir98@gmail.com>
    
    * Updated README.md with proto compile and install guide.
    
    Signed-off-by: omerzamir <zamir98@gmail.com>
    
    * create Makefile, update README.md, clean go.mod
    
    * change UploadFile to get io.Reader and update protobuf
    
    * grpc server is functional
    
    * formatting
    
    Signed-off-by: omerzamir <zamir98@gmail.com>
    
    * first test passes, updated Makefile
    
    * add sudo before rm in Makefile
    
    * add test case for uploading a file to a folder
    
    * add test cases for UploadFile
    
    * add multipart upload and updated tests
    
    * formatting
    
    Signed-off-by: omerzamir <zamir98@gmail.com>
    
    * added test case for UploadMedia and init function for tests
    
    Signed-off-by: omerzamir <zamir98@gmail.com>
    
    * added test cases
    
    Signed-off-by: omerzamir <zamir98@gmail.com>
    
    * multipart upload request implemented in rpc
    
    * changed test to run with buffconn to mock grpc connection
    
    Signed-off-by: omerzamir <zamir98@gmail.com>
    
    * changed the MaxRecvMsgSize to 10M
    
    Signed-off-by: omerzamir <zamir98@gmail.com>
    
    * changed the MaxRecvMsgSize to Real 10M
    
    Signed-off-by: omerzamir <zamir98@gmail.com>
    
    * resumable upload in protobuf
    
    * resumable upload partially implemented
    
    * upload resumable switch case
    
    * resumable upload implemented
    
    * added test stubs for handler and service functions
    
    Signed-off-by: omerzamir <zamir98@gmail.com>
    
    * documentation for upload functions
    
    * fix UploadPart possible nil pointer exception
    
    Signed-off-by: omerzamir <zamir98@gmail.com>
    
    * check if metadata exist in upload init - required field
    
    Signed-off-by: omerzamir <zamir98@gmail.com>
    
    * added UploadInit tests for handler and service
    
    Signed-off-by: omerzamir <zamir98@gmail.com>
    
    * protoc install script and updated README.md
    
    * <<
    
    Signed-off-by: omerzamir <zamir98@gmail.com>
    
    * dockerfile builds successfully
    
    * added test for UploadPart
    
    Signed-off-by: omerzamir <zamir98@gmail.com>
    
    * added tests for UploadInit service function
    
    Signed-off-by: omerzamir <zamir98@gmail.com>
    
    * add TCP_PORT env in docker-compose
    
    Signed-off-by: Omri Shtamberger <omrishtam@gmail.com>
    
    * added tests for UploadComplete service
    
    Signed-off-by: omerzamir <zamir98@gmail.com>
    
    * added test case for nil and empty id in UploadComplete
    
    Signed-off-by: omerzamir <zamir98@gmail.com>
    
    * added test cases that were missed and added stub for UploadMultipart
    
    Signed-off-by: omerzamir <zamir98@gmail.com>
    
    * added test cases for UploadMultipart handler and UploadFile service and added check if file is null in UploadFile Service
    
    Signed-off-by: omerzamir <zamir98@gmail.com>
    
    * add UploadAbort and usage of context in s3 operations
    
    Signed-off-by: Omri Shtamberger <omrishtam@gmail.com>
    
    * UploadAbort rpc handler
    
    Signed-off-by: omerzamir <zamir98@gmail.com>
    
    * fixed tests
    
    Signed-off-by: omerzamir <zamir98@gmail.com>
    
    * fixed tests - file too big
    
    Signed-off-by: omerzamir <zamir98@gmail.com>
    
    * ensure bucket exists before any upload service operation
    
    Signed-off-by: Omri Shtamberger <omrishtam@gmail.com>
    
    * Feature/bucket tests (#1)
    
    * added test cases for create bucket
    
    Signed-off-by: omerzamir <zamir98@gmail.com>
    
    * added test cases for bucket exists
    
    Signed-off-by: omerzamir <zamir98@gmail.com>

---
+++ go.mod
@@ -0,0 +1,11 @@
+module upload-service
+
+go 1.12
+
+require (
+	github.com/aws/aws-sdk-go v1.18.2
+	github.com/golang/protobuf v1.3.0
+	github.com/gorilla/mux v1.7.0
+	golang.org/x/net v0.0.0-20190313220215-9f648a60d977
+	google.golang.org/grpc v1.19.0
+)
---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"log"
+	"net"
+	"os"
+	pb "upload-service/proto"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	s3AccessKey := os.Getenv("S3_ACCESS_KEY")
+	s3SecretKey := os.Getenv("S3_SECRET_KEY")
+	s3Endpoint := os.Getenv("S3_ENDPOINT")
+	tcpPort := os.Getenv("TCP_PORT")
+	s3Token := ""
+
+	// Configure to use S3 Server
+	s3Config := &aws.Config{
+		Credentials:      credentials.NewStaticCredentials(s3AccessKey, s3SecretKey, s3Token),
+		Endpoint:         aws.String(s3Endpoint),
+		Region:           aws.String("eu-east-1"),
+		DisableSSL:       aws.Bool(true),
+		S3ForcePathStyle: aws.Bool(true),
+	}
+	newSession := session.New(s3Config)
+	s3Client := s3.New(newSession)
+	lis, err := net.Listen("tcp", ":"+tcpPort)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer(grpc.MaxRecvMsgSize(10 << 20))
+	server := &UploadHandler{UploadService: UploadService{s3Client: s3Client}}
+	pb.RegisterUploadServer(grpcServer, server)
+	grpcServer.Serve(lis)
+}
