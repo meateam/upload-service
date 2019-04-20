930ebd8d8cf6405929e6eda463f404ae31ce579b
Omri Shtamberger <omrishtam@gmail.com>
2019-04-20 19:56:55 +0300

    increase gRPC Max Receive Message Size from 10MB to 5GB
    Signed-off-by: Omri Shtamberger <omrishtam@gmail.com>

--- main.go
+++ main.go
@@ -35,7 +35,7 @@ func main() {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
-	grpcServer := grpc.NewServer(grpc.MaxRecvMsgSize(10 << 20))
+	grpcServer := grpc.NewServer(grpc.MaxRecvMsgSize(5000 << 20))
 	server := &UploadHandler{UploadService: UploadService{s3Client: s3Client}}
 	pb.RegisterUploadServer(grpcServer, server)
 	grpcServer.Serve(lis)
